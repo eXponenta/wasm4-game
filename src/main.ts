3d12a39c0a8050cb8f1d6bfa41cfc7a6fa64efca
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-09-03 00:07:58 +0300

    basic game setup

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,84 @@
+import { treeFrame } from "./img/tree";
+import { Frame, Sprite } from "./nodes/sprite";
 import * as w4 from "./wasm4";
 
-const COUNT = 100;
-const array: StaticArray<i32> = new StaticArray(COUNT * 2);
+store<u32>(w4.PALETTE, 0x0, 0 * sizeof<u32>()) // 1
+store<u32>(w4.PALETTE, 0xffffff, 1 * sizeof<u32>()) // 2
+store<u32>(w4.PALETTE, 0x00ff00, 2 * sizeof<u32>()) // 3
+store<u32>(w4.PALETTE, 0x0000ff, 3 * sizeof<u32>()) //4
 
-for(let i = 0; i < array.length; i += 2) {
-    array[i] = i32(Math.random() * w4.SCREEN_SIZE);
-    array[i + 1] = i32(Math.random() * w4.SCREEN_SIZE);
+const COUNT = 100;
+const array: Array<Sprite> = new Array<Sprite>(COUNT + 1);
+
+const smiley = memory.data<u8>([
+    0b11000011,
+    0b10000001,
+    0b00100100,
+    0b00100100,
+    0b00000000,
+    0b00100100,
+    0b10011001,
+    0b11000011,
+]);
+
+const player = new Sprite(new Frame(8, 8, smiley, w4.BLIT_1BPP));
+const spriteFrame = treeFrame;
+
+for(let i = 0; i < COUNT; i++) {
+    const sprite = new Sprite(spriteFrame);
+    
+    sprite.x = f32(i32(Math.random() * (w4.SCREEN_SIZE - 8))) + 8.;
+    sprite.y = f32(i32(Math.random() * (w4.SCREEN_SIZE - 16))) + 16.;
+
+    array[i] = sprite;
 }
+
+array[COUNT] = player;
+
+player.x = f32(w4.SCREEN_SIZE / 2);
+player.y = f32(w4.SCREEN_SIZE);
+
+function sortSpriteY(a: Sprite, b: Sprite): i32 {
+    return i32(a.y - b.y);
+}
+
 
 export function start(): void {
 
 }
 
 export function update (): void {
+    const gamepad = load<u8>(w4.GAMEPAD1);
 
+    let dx: i32 = 0;
+    let dy: i32 = 0;
+
+    if (gamepad & w4.BUTTON_RIGHT) {
+        dx = 1;
+    } else if (gamepad & w4.BUTTON_LEFT) {
+        dx = -1;
+    }
+
+    if (gamepad & w4.BUTTON_UP) {
+        dy = -1;
+    } else if (gamepad & w4.BUTTON_DOWN) {
+        dy = 1;
+    }
+    
+    player.x += f32(dx) * 0.5;
+    player.y += f32(dy) * 0.5;
+
+    array.sort(sortSpriteY);
+
+    for(let i = 0; i < array.length; i ++) {
+
+        if (array[i] == player) {
+            store<u16>(w4.DRAW_COLORS, 0x04);
+        } else {
+            // tree
+            store<u16>(w4.DRAW_COLORS, 0x4230);
+        }
+
+        array[i].draw();
+    }    
 }
--- src/nodes/sprite.ts
+++ src/nodes/sprite.ts
@@ -1,34 +1,45 @@
 import * as w4 from './../wasm4';
 
+@unmanaged
+export class Frame {
+    constructor (
+        public width: u8,
+        public height: u8,
+        public data: usize,
+        public type: u32 = w4.BLIT_1BPP
+    ) {
+    }
+}
+
 @final
 @unmanaged
 export class Sprite {
-    public x: i32 = 0;
-    public y: i32 = 0;
+    public x: f32 = 0;
+    public y: f32 = 0;
 
     public flipX: u32 = 0;
     public flipY: u32 = 0;
 
     constructor (
-        public width: u32 = 0,
-        public height: u32 = 0,
-        public data: usize,
-        public type: usize = w4.BLIT_1BPP
+        public frame: Frame
     ) {
-
     }
 
-    public draw() {
+    public draw(): void {
+        const frame: Frame = this.frame;
+        const x = this.x - f32(frame.width / 2);
+        const y = this.y - f32(frame.height);
+
         w4.blit(
-            this.data,
-            this.x,
-            this.y,
-            this.width,
-            this.height,
+            frame.data,
+            i32(x),
+            i32(y),
+            frame.width,
+            frame.height,
             (
                 this.flipX & w4.BLIT_FLIP_X |
                 this.flipY & w4.BLIT_FLIP_Y |
-                this.type
+                frame.type
             )
         );
     }
